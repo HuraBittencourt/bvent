06d2b8e34aa95e50e8036fb0230299b7cade1d8b
Euclydes <euclydesbsn@gmail.com>
2018-08-20 02:03:42 -0300

    scanner lendo qrcode e enviando para o firebase

---
+++ public/scan/js/main.js
@@ -0,0 +1,48 @@
+let scanner = new Instascan.Scanner(
+    {
+        video: document.getElementById('preview')
+    }
+);
+
+scanner.addListener('scan', function(content){
+    let scannedCode = content.split(',');
+    writeUserData(scannedCode);
+    console.log("QR Code lido com sucesso!");
+});
+
+Instascan.Camera.getCameras().then(cameras =>
+{
+    if(cameras.length > 0){
+        scanner.start(cameras[0]);
+    }else{
+        console.error("Não existe câmera no dispositivo");
+    }
+})
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDlYN9L9RjetF1PN8BEWXcMSxQOArC1FNc",
+    authDomain: "bvent-55e01.firebaseapp.com",
+    databaseURL: "https://bvent-55e01.firebaseio.com",
+    projectId: "bvent-55e01",
+    storageBucket: "bvent-55e01.appspot.com",
+    messagingSenderId: "484288242152"
+};
+firebase.initializeApp(config);
+
+//Referenciar
+var database = firebase.database();
+
+//Gravar dados no firebase
+function writeUserData(scannedCode) {
+firebase.database().ref('leads/' + scannedCode[6]).set({
+    //feira,empresaStand,Euclydes,Developer,Microsoft,euclydesbsn@gmail.com,13996221752
+    feira: scannedCode[0],
+    stand: scannedCode[1],
+    nome : scannedCode[2],
+    cargo : scannedCode[3],
+    empresa : scannedCode[4],
+    email : scannedCode[5],
+    telefone : scannedCode[6]
+});
+}
